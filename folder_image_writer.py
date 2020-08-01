458d1c585768d6721e7314656877ead0520b7127
Bryan Dormaier <bryan.dormaier@gmail.com>
2020-08-01 09:25:25 -0700

    add implementation for browsing on web

---
+++ folder_image_writer.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import os
+
+dirs = ["ABPI", "BTAS", "BWM", "CAAV", "CMM", "EX", "F4", "FFCC", "JLU", "Markers", "ORVL", "RE", "STRF", "SWBW", "WCR", "WK", "WWE", "XDPS"]
+
+for dir in dirs:
+    f = open(dir + "/images.html", "w")
+    f.write("<!DOCTYPE html><html lang=\"en-us\"><head><meta charset=\"utf-8\"><title>Heroclix Map Viewer</title><style type=\"text/css\">.container {display: flex;flex-wrap: wrap; justify-content:center;} .roll20 {height:180px; width:auto; padding-top:5px; padding-bottom:5px; padding-left:5px; padding-right:5px;} </style></head>")
+
+    f.write ("<body><div class=\"container\">")
+
+    for path in Path(dir).rglob('*.png'):
+        f.write("<img class=\"roll20\" src=\"")
+        f.write(path.name)
+        f.write("\"/>")
+
+    f.write("</div></body></html>")
