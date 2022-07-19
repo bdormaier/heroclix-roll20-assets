d3ce3503879ac65bb4aaea14e95d42e3584caaa4
Bryan Dormaier <bryan.dormaier@gmail.com>
2022-07-18 19:30:19 -0700

    update python to not need to list directory names

--- folder_image_writer.py
+++ folder_image_writer.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import os
 
-dirs = ["XOS", "DPLUS",  "WOR", "F4SLOP", "AFFE", "EM", "XRF", "WW80", "FFFF", "F4DC", "HOX", "ABPI", "BTAS", "BWM", "CAAV", "CMM", "EX", "F4", "FFCC", "JLU", "Markers", "ORVL", "RE", "STRF", "SWBW", "WCR", "WK", "WWE", "XDPS", "STBG", "BGAM", "DCXM", "SVAC"]
-
+#dirs = ["XOS", "DPLUS",  "WOR", "F4SLOP", "AFFE", "EM", "XRF", "WW80", "FFFF", "F4DC", "HOX", "ABPI", "BTAS", "BWM", "CAAV", "CMM", "EX", "F4", "FFCC", "JLU", "Markers", "ORVL", "RE", "STRF", "SWBW", "WCR", "WK", "WWE", "XDPS", "STBG", "BGAM", "DCXM", "SVAC"]
+dirs = filter(os.path.isdir, os.listdir(os.curdir))
 for dir in dirs:
     f = open(dir + "/images.html", "w")
     f.write("<!DOCTYPE html><html lang=\"en-us\"><head><meta charset=\"utf-8\"><title>Heroclix Map Viewer</title><style type=\"text/css\">.container {display: flex;flex-wrap: wrap; justify-content:center;} .roll20 {height:180px; width:auto; padding-top:5px; padding-bottom:5px; padding-left:5px; padding-right:5px;} </style></head>")
